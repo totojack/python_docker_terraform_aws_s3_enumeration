d6b12a8d36153e4478b73ed56ac1e2777b37ecde
Luca Iovio <luca.iovio@gmail.com>
2022-05-31 16:11:46 +0000

    [feature/adding-plint] linting

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
-import boto3
+# pylint: disable=missing-module-docstring
 import os
+import boto3
 
-session = boto3.Session( 
-         aws_access_key_id=os.getenv('AWS_ACCESS_KEY'), 
+session = boto3.Session(
+         aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
          aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))
 
 s3 = session.resource('s3')
